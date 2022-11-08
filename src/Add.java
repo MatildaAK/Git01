95875fbc0a65816a89b4de76be865ca9423118c0
MatildaKällström <matilda.kallstrom@hotmail.se>
2022-11-08 14:37:19 +0100

    Add Age

--- src/Add.java
+++ src/Add.java
@@ -3,10 +3,15 @@ import java.util.Scanner;
 public class Add {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Hello, what´s your name: ");
         String name = scanner.next();
         System.out.println("Your name is " + name + ".");
+
+        System.out.println();
+
+        System.out.print("Age: ");
+        int age = scanner.nextInt();
+        System.out.println("You are " + age + " year old.");
     }
 
 }
