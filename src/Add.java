cf04f5eb81c4a8e909b196caa8b8ebfa8d1227f4
MatildaKällström <matilda.kallstrom@hotmail.se>
2022-11-08 14:53:27 +0100

    add higth

--- src/Add.java
+++ src/Add.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Add {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("Hello, what´s your name: ");
         String name = scanner.next();
         System.out.println("Your name is " + name + ".");
@@ -12,6 +13,12 @@ public class Add {
         System.out.print("Age: ");
         int age = scanner.nextInt();
         System.out.println("You are " + age + " year old.");
+
+        System.out.println();
+
+        System.out.print("How tall in you in centimeter: ");
+        double higth = scanner.nextDouble();
+        System.out.println("you are: " + higth + " centimeter tall.");
     }
 
 }
