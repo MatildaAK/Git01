718e14e24815b77db47070de4c5de0417d9eb09c
MatildaKällström <matilda.kallstrom@hotmail.se>
2022-11-09 11:19:54 +0100

    New test

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("Change again");
         System.out.println("Change 4 and 6");
         System.out.println("Change 8");
+        System.out.println("This is a new test");
     }
 }
