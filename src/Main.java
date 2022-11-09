8ae66e8315af9a2d97b3b9fa0d8fc3451e877a93
MatildaKällström <matilda.kallstrom@hotmail.se>
2022-11-09 10:14:33 +0100

    Change 4

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,6 @@ public class Main {
         System.out.println("Hej hej");
         System.out.println("Hello world! Change from clone");
         System.out.println("Change again");
-        System.out.println("Change 1");
+        System.out.println("Change 4");
     }
 }
